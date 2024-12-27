bb7b81b7813c493a2c687665fbe93fe4100fc477
Kiriil <kirillprofi007@gmail.com>
2024-12-27 20:12:04 +0300

    ft: add migrations

---
+++ prisma/seed.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/shared/lib/db";
+
+async function main() {}
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
